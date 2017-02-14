3d2e6a731a8a63db662229e683dbfabb24258204
jotespeh <jakob@Ab-al-Jakob.local>
2017-02-14 22:26:17 +0100

    Fixes for scrape_laender
    -Fix percentages
    -Fix Berlin dates
    -Remove clutter from list

--- R/scrape_laender.R
+++ R/scrape_laender.R
@@ -1,75 +1,103 @@
+scrape_laender <- function(list = FALSE) {
+  library(lubridate)
+  library(magrittr)
+  library(rvest)
+  library(stringr)
+  library(tidyverse)
+
+  #TODO:filter sonstiges, make AfD regex work if AfD ist not at beginning of string
+
+  # Scrape Laender polls page, select second table, which contains poll data then coerce into dataframe
+  lwa <- "http://www.wahlrecht.de/umfragen/laender.htm" %>%
+    read_html() %>%
+    html_nodes("table") %>%
+    .[[2]] %>%
+    html_table(fill = TRUE)
+
+  # Find header rows for the 16 Laender using regex, clean up columns
+  lwa %<>%
+    mutate(
+      header   = ifelse(grepl("(Bundestagswahl)", CDU), TRUE, FALSE),
+      institut = gsub("\\(.*$", "",  x = `Institut(Datum)`),
+      # Somewhat ugly fix for Berlin polling
+      datum    = dmy(ifelse(
+        grepl("\\(", x = `Institut(Datum)`),
+        gsub(".*\\((.*)\\).*", "\\1", x = `Institut(Datum)`),
+        str_sub(`Institut(Datum)`, -10, -1)
+      )),
+
+      befragte = as.numeric(ifelse(
+        str_sub(BefragteZeitraum, 1, 1) <= 2,
+        gsub("[.]", "", str_sub(BefragteZeitraum, 1, 5)),
+        str_sub(BefragteZeitraum, 1, 3)
+      )),
+      afd      = as.numeric(ifelse(
+        grepl("AfD", Sonstige) | grepl("\\%AfD", Sonstige),
+        gsub("^[^AfD]*AfD(*[^%]+).*", "\\1", Sonstige),
+        NA
+      ))
+    ) %>%
+    select(
+      header,
+      datum,
+      institut,
+      befragte,
+      cdu = CDU,
+      spd = SPD,
+      gru = `GRÜNE`,
+      lin = LINKE,
+      fdp = FDP,
+      afd,
+      sonstige = Sonstige
+    ) %>%
+    filter(!grepl("Bundestagswahl", .$institut), !grepl("Institut", .$institut))
+
+ # Percentage-string to numeric, loses precision, but no poll > 2013 has decimal
+ lwa %<>% map_at(c("cdu", "spd", "gru", "lin", "fdp", "fdp"),
+                str_extract, pattern = "\\d{1,2}") %>%
+          map_at(c("cdu", "spd", "gru", "lin", "fdp", "fdp"), as.numeric) %>%
+          as_tibble()
 
 
-scrape_laender <- function(laender = all, list = FALSE) {
+  # Get vector of rownumbers for splitting 
+  # Splitting is necessary, as all laender are in one table
+  lae_rn <- rownames(lwa[lwa$header == TRUE,])
 
-library(lubridate)
-library(rvest)
-library(stringr)
-library(tidyverse)
+  # Split dataframe into list, using rownumbers 
+  tab_bw <- split(lwa, cumsum(1:nrow(lwa) %in% lae_rn))
 
-#TODO: string->numeric for parties, fix datum for Berlin >2013, filter sonstiges
+  # Delete first df which only contains info that we don't need, then assign names
+  tab_bw[1] <- NULL
+  names(tab_bw) <-
+    c(
+      "BW",
+      "BY",
+      "BE",
+      "BB",
+      "HB",
+      "HH",
+      "HE",
+      "MV",
+      "NI",
+      "NW",
+      "RP",
+      "SL",
+      "SN",
+      "ST",
+      "SH",
+      "TH"
+    )
 
-# Scrape Laender polls page, select second table, which contains poll data then
-# coerce into dataframe
-lwa <- "http://www.wahlrecht.de/umfragen/laender.htm" %>%
-  read_html() %>%
-  html_nodes("table") %>%
-  .[[2]] %>%
-  html_table(fill = TRUE)
-
-# Find Headers for the 16 Laender using regex, clean up columns
-lwa %<>%
-  mutate(
-    header   = ifelse(grepl("(Bundestagswahl)", CDU), TRUE, FALSE),
-    institut = gsub( "\\(.*$", "",  x = `Institut(Datum)`),
-    # Somewhat ugly fix for Berlin polling
-    # TODO : Datum für Berlin
-    datum    = dmy(gsub( ".*\\((.*)\\).*", "\\1", x = `Institut(Datum)`)),
-    befragte = as.numeric(ifelse(
-                            str_sub(BefragteZeitraum,1,1) <= 2,
-                            gsub( "[.]", "", str_sub(BefragteZeitraum, 1, 5)),
-                            str_sub(BefragteZeitraum, 1, 3)
-                            )
-                          ),
-    afd      = ifelse(grepl("AfD", Sonstige),
-                        gsub("^[^AfD]*AfD([^%]+).*", "\\1", Sonstige)
-                        , NA)
-  ) %>%
-  select(
-    header,
-    datum,
-    institut,
-    befragte,
-    cdu = CDU,
-    spd = SPD,
-    gru = `GRÜNE`,
-    lin = LINKE,
-    fdp = FDP,
-    afd,
-    sonstige = Sonstige
-  )
-
-
-# lwa %<>% map_at(c("cdu", "spd", "gru", "lin", "fdp", "fdp", "afd"), pct2num)
-
-
-# Get list of rownumbers for splitting
-lae_rn <- rownames(lwa[lwa$header == TRUE, ])
-
-# Split dataframe into list
-tab_bw <- split(lwa, cumsum(1:nrow(lwa) %in% lae_rn))
-
-# Delete first df which only contains info that we don't need, then assign names
-tab_bw[1] <- NULL
-names(tab_bw) <- c("BW", "BY", "BE", "BB", "HB", "HH", "HE", "MV", "NI", "NW", "RP", "SL", "SN", "ST", "SH", "TH")
-
-if (list == FALSE) {
-  # Bind into one dataframe, add identifier and filter garbage
-  tab_bw %<>% bind_rows(.id = "land")
-  tab_bw %<>% filter(befragte < 2001 & !grepl("Bundestagswahl", institut))
-  return(tab_bw)
-}
-else
-  return(tab_bw)
+  tab_bw %<>%
+    map(~filter(., .$header == FALSE)) %>%
+    map(~select(., -header))
+    
+  if (list == FALSE) {
+    # Bind into one dataframe (or rather tibble)
+    tab_bw %<>% bind_rows(.id = "land")
+    return(tab_bw)
+  }
+  else
+    return(tab_bw)
 
 }
